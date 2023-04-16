f9dba43e795d6fef986eb0fd9a4a5eaefaee5400
enesdoganoglu <117980244+enesdoganoglu@users.noreply.github.com>
2023-04-17 00:03:07 +0300

    second commit

--- YemekSepetiApp/src/main/java/com/bilgeadam/repository/joinrepo/IOrderCustomerRepository.java
+++ YemekSepetiApp/src/main/java/com/bilgeadam/repository/joinrepo/IOrderCustomerRepository.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IOrderCustomerRepository {
-    @Query(nativeQuery = true, value =
-            "select p.product_id, o.order_id,r.restaurant_id,c.customer_name,c.customer_surname,c.customer_address,p.product_name,r.restaurant_name from tbl_order as o \n" +
-                    "inner join order_product_id as op on op.order_order_id\n" +
-                    "inner join tbl_product as p on p.product_id=op.product_id\n" +
-                    "inner join tbl_customer as c on c.customer_id=o.customer_id\n" +
-                    "inner join tbl_restaurant as r on r.restaurant_id=p.restaurant_id\n" +
-                    "where r.restaurant_id=?1")
+    @Query(nativeQuery = true, value = "select c.customer_name ,c.customer_surname, opi.order_order_id, r.restaurant_name , p.product_name from customer as c \n" +
+            "inner join tblorder as tblo on tblo.customer_id = c.customer_id\n" +
+            "inner join restaurant as r on r.restaurant_id = tblo.restaurant_id\n" +
+            "inner join order_product_id as opi on opi.order_order_id = tblo.order_id\n" +
+            "inner join product as p on p.product_id = opi.product_id where c.customer_id = ?1")
     List<OrderCustomer> customerOrders(Long customerId);
 }
